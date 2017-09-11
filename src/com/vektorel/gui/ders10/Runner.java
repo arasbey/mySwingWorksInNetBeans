4fc90be544d854ab09187e8c68c4d35bf354723e
Burak ARAS <31282950+arasbey@users.noreply.github.com>
2017-09-11 14:37:24 +0300

    Initial commit

---
+++ src/com/vektorel/gui/ders10/Runner.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vektorel.gui.ders10;
+
+
+/**
+ *
+ * @author vektorel
+ */
+public class Runner {
+    
+    public static void main(String[] args) {
+        FrmLogin frmLogin=new FrmLogin();
+        frmLogin.setLocationRelativeTo(null);
+        frmLogin.setTitle("Algo V1.0.0");
+        frmLogin.show();
+    }
+    
+}
